d2d975f11111c3888dd251fd1b750d8f01cbbfa0
Josh <80072793+thejosharsen@users.noreply.github.com>
2021-08-14 15:44:05 -0300

    Update db.uspZooDBAssignment.sql
    * Fixed incorrect inner join

--- SQL/ZooDatabase/db.uspZooDBAssignment.sql
+++ SQL/ZooDatabase/db.uspZooDBAssignment.sql
@@ -27,7 +27,7 @@ WHERE species_nutrition BETWEEN 2202 AND 2206;
 
 SELECT species_name AS [Species Name], nutrition_type AS [Nutrition Type]
 FROM tbl_species
-INNER JOIN tbl_nutrition ON tbl_nutrition.nutrition_id = tbl_species.species_id;
+INNER JOIN tbl_nutrition ON tbl_nutrition.nutrition_id = tbl_species.species_nutrition;
 
 /* Assignment 6 */
 
