4d0d128cd6015c50221f6e7c7d86dafce20c8d9f
Josh <80072793+thejosharsen@users.noreply.github.com>
2021-08-16 09:41:07 -0300

    Update db.uspFindBorrowersWithNoBooksOut.sql
    * Added new query

--- SQL/LibraryManagementSystem/db.uspFindBorrowersWithNoBooksOut.sql
+++ SQL/LibraryManagementSystem/db.uspFindBorrowersWithNoBooksOut.sql
@@ -1,3 +1,7 @@
 CREATE PROCEDURE FindBorrowersWithNoBooksOut
 AS
 
+SELECT [Name], [Address], [Phone]
+FROM Borrower
+FULL OUTER JOIN Book_Loans ON Borrower.CardNo=Book_Loans.CardNo
+WHERE DateDue IS NULL;
