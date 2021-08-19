7bf5f383d785e6b800631b37a9b8c254998e370d
Josh <80072793+thejosharsen@users.noreply.github.com>
2021-08-19 15:39:42 -0300

    Added VariablesAndDataTypes

---
+++ Basic C-Sharp Projects/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favourite number?");
+            //string favouriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favouriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favourite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMarks = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
