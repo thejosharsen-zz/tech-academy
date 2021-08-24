07ba0d4e31603853d1237ba48465b5694bda78c7
Josh <80072793+thejosharsen@users.noreply.github.com>
2021-08-24 19:02:27 -0300

    Added LoopAssignment

---
+++ Basic C-Sharp Projects/LoopAssignment/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a number between 1 and 10");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            bool correctNumber = userNumber == 5;
+
+            // Exception for when users enter a number > 10
+            while (userNumber > 10)
+            {
+                Console.WriteLine("Your number does not meet the guidelines. Goodbye!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            // Otherwise... 
+            do
+            {
+                switch (userNumber)
+                {
+                    case 1:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        correctNumber = true;
+                        Console.WriteLine("That number is CORRECT");
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.WriteLine("That number is incorrect");
+                        Console.WriteLine("Pick a number between 1 and 10");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!correctNumber);
+        }
+    }
+}
